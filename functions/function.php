e56d4444aaf929a9158e53a297fe33f54ad6e2ba
Abdullah Al Mamun <145656875+mamundevcode@users.noreply.github.com>
2023-09-25 09:43:38 +0600

    Add files via upload

---
+++ functions/function.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('config.php');
+function get_header(){
+
+    require_once ('includes/header.php');
+}
+function get_sitebar(){
+
+    require_once ('./includes/sitebar.php');
+}
+
+function get_footer(){
+                                               
+    require_once ('./includes/footer.php');
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require_once('functions/function.php');
+get_header();
+get_sitebar();
+
+?>
+    
+                    <div class="row">
+                        <div class="col-md-12 welcome_part">
+                            <p><span>Welcome Mr.</span> Abdullah all Mamun</p>
+                        </div>
+                    </div>
+ <?php
+ 
+        get_footer();
+ ?>
